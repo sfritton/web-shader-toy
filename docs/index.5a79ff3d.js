a195b9486410471de000e1132a0dd3d65629022e
Sam Fritton <samuel.fritton@target.com>
2024-02-07 13:25:47 -0600

    Publish

--- docs/index.5a79ff3d.js
+++ docs/index.5a79ff3d.js
@@ -1,3 +1,12 @@
+
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
+// TODO: update config so /index isn't necessary
 class $50ad2468206126ad$export$462bb059fed9d9e5 {
     calculateDeltaT(currMS) {
         if (this.prevMS === undefined) this.prevMS = currMS;
@@ -81,7 +90,7 @@ class $50ad2468206126ad$export$462bb059fed9d9e5 {
 }
 
 
-const $4e939e94d233ed5b$export$eb912ff306213ab3 = (radius, angle)=>{
+const $a208a15eab1643c4$export$eb912ff306213ab3 = (radius, angle)=>{
     const x = radius * Math.cos(angle);
     const y = radius * Math.sin(angle);
     return {
@@ -89,50 +98,82 @@ const $4e939e94d233ed5b$export$eb912ff306213ab3 = (radius, angle)=>{
         y: y
     };
 };
-const $4e939e94d233ed5b$export$c4293a6a9b6ab33a = (radius)=>{
+const $a208a15eab1643c4$export$c4293a6a9b6ab33a = (radius)=>{
     const r = Math.sqrt(Math.random()) * radius;
     const angle = Math.random() * 2 * Math.PI;
-    return $4e939e94d233ed5b$export$eb912ff306213ab3(r, angle);
+    return $a208a15eab1643c4$export$eb912ff306213ab3(r, angle);
 };
 
 
-const $c8a6c04c3ac47780$var$WORKGROUP_SIZE = 64;
-const $c8a6c04c3ac47780$var$PARTICLE_SIZE = 0.1;
-const $c8a6c04c3ac47780$var$PARTICLE_COUNT = $c8a6c04c3ac47780$var$WORKGROUP_SIZE * 128;
-const $c8a6c04c3ac47780$var$SPEED = 0.004;
-const $c8a6c04c3ac47780$var$GRAVITY = 0.005;
-const $c8a6c04c3ac47780$var$PARTICLE_INTERVAL = 5;
-const $c8a6c04c3ac47780$var$DECAY_RATE = 0.004;
-const $c8a6c04c3ac47780$var$ORIGIN_RADIUS = 0.2;
-const $c8a6c04c3ac47780$var$ORIGIN_Y = -0.4;
-const $c8a6c04c3ac47780$var$TRIANGLE_COUNT = 16;
-const $c8a6c04c3ac47780$var$THETA = 2 * Math.PI / $c8a6c04c3ac47780$var$TRIANGLE_COUNT;
-const $c8a6c04c3ac47780$var$VERTICES = new Float32Array($c8a6c04c3ac47780$var$TRIANGLE_COUNT * 6); // 6 for the x,y coordinates of the three points on the triangle
-for(let triangleIndex = 0; triangleIndex < $c8a6c04c3ac47780$var$TRIANGLE_COUNT; triangleIndex++){
+var $1ba33acf88ea8827$exports = {};
+
+$parcel$export($1ba33acf88ea8827$exports, "WORKGROUP_SIZE", function () { return $1ba33acf88ea8827$export$fb78680ee3bd0d21; });
+$parcel$export($1ba33acf88ea8827$exports, "PARTICLE_SIZE", function () { return $1ba33acf88ea8827$export$66983b8483285905; });
+$parcel$export($1ba33acf88ea8827$exports, "PARTICLE_COUNT", function () { return $1ba33acf88ea8827$export$ffc0241869fe445; });
+$parcel$export($1ba33acf88ea8827$exports, "SPEED", function () { return $1ba33acf88ea8827$export$cc060c12b7723909; });
+$parcel$export($1ba33acf88ea8827$exports, "GRAVITY", function () { return $1ba33acf88ea8827$export$9c83558f29ac851e; });
+$parcel$export($1ba33acf88ea8827$exports, "PARTICLE_INTERVAL", function () { return $1ba33acf88ea8827$export$9065f2e29538d195; });
+$parcel$export($1ba33acf88ea8827$exports, "DECAY_RATE", function () { return $1ba33acf88ea8827$export$f8d487931b715cc9; });
+$parcel$export($1ba33acf88ea8827$exports, "ORIGIN_RADIUS", function () { return $1ba33acf88ea8827$export$ff0d559910d80bb9; });
+$parcel$export($1ba33acf88ea8827$exports, "ORIGIN_Y", function () { return $1ba33acf88ea8827$export$28b92f9a30b92e0b; });
+const $1ba33acf88ea8827$export$fb78680ee3bd0d21 = 64;
+const $1ba33acf88ea8827$export$66983b8483285905 = 0.1;
+const $1ba33acf88ea8827$export$ffc0241869fe445 = $1ba33acf88ea8827$export$fb78680ee3bd0d21 * 128;
+const $1ba33acf88ea8827$export$cc060c12b7723909 = 0.004;
+const $1ba33acf88ea8827$export$9c83558f29ac851e = 0.005;
+const $1ba33acf88ea8827$export$9065f2e29538d195 = 5;
+const $1ba33acf88ea8827$export$f8d487931b715cc9 = 0.004;
+const $1ba33acf88ea8827$export$ff0d559910d80bb9 = 0.2;
+const $1ba33acf88ea8827$export$28b92f9a30b92e0b = -0.4;
+
+
+
+var $1c923f75c1506daf$exports = {};
+$1c923f75c1506daf$exports = "struct VertexInput {\n  @location(0) pos: vec2f,\n  @builtin(instance_index) instance: u32,\n};\n\nstruct VertexOutput {\n  @builtin(position) pos: vec4f,\n  @location(0) lifespan: f32,\n  @location(1) alpha: f32,\n};\n\n@group(0) @binding(0) var<storage> particleStates: array<f32>;\n\n// Returns a y value between 0 and 1\nfn lerp(x: f32, x1: f32, x2: f32, y1: f32, y2: f32) -> f32 {\n  return y1 + (x - x1) * (y2 - y1) / (x2 - x1);\n}\n\nfn particleScale(lifespan: f32) -> f32 {\n  let fullSizeBegin = 0.85;\n  let fullSizeEnd = 0.8;\n\n  if (lifespan > fullSizeBegin) {\n    return lerp(lifespan, 1, fullSizeBegin, 0, 1);\n  } else if (lifespan > fullSizeEnd) {\n    return 1;\n  } else {\n    return lerp(lifespan, fullSizeEnd, 0, 1, 0);\n  }\n}\n\n@vertex\nfn vertexMain(input: VertexInput) -> VertexOutput {\n  let i = input.instance * ${PARTICLE_INTERVAL};\n  let position = vec2f(particleStates[i], particleStates[i+1]);\n  let lifespan = particleStates[i+4];\n  let scale = particleScale(lifespan);\n  let pos = (input.pos * ${PARTICLE_SIZE} * scale) + position;\n\n  var output: VertexOutput;\n  output.pos = vec4f(pos, 0, 1);\n  output.lifespan = lifespan;\n  if (input.pos.x == 0 && input.pos.y == 0) {\n    output.alpha = scale;\n  } else {\n    output.alpha = 0;\n  }\n  return output;\n}\n\n@fragment\nfn fragmentMain(input: VertexOutput) -> @location(0) vec4f {\n  return vec4f(0.4 + input.lifespan * 2, input.lifespan * 1.5, input.lifespan * 2 - 0.75, 1) * input.alpha; // rgba\n}";
+
+
+var $45af1b9190be3989$exports = {};
+$45af1b9190be3989$exports = "@group(0) @binding(0) var<storage> particleStatesIn: array<f32>;\n@group(0) @binding(1) var<storage, read_write> particleStatesOut: array<f32>;\n@group(0) @binding(2) var<storage> initialStates: array<f32>;\n\n@compute\n@workgroup_size(${WORKGROUP_SIZE})\nfn computeMain(@builtin(global_invocation_id) index: vec3u) {\n  let i = index.x * ${PARTICLE_INTERVAL};\n  let positionIn = vec2f(particleStatesIn[i], particleStatesIn[i+1]);\n  let velocityIn = vec2f(particleStatesIn[i+2], particleStatesIn[i+3]);\n  let lifespan = particleStatesIn[i+4] - ${DECAY_RATE};\n\n  let positionOut = positionIn + velocityIn * ${SPEED};\n  let velocityOut = vec2f(velocityIn.x, velocityIn.y + ${GRAVITY});\n\n  // reset the particle\n  if (lifespan < 0) {\n    particleStatesOut[i] = initialStates[i];\n    particleStatesOut[i+1] = initialStates[i+1];\n    particleStatesOut[i+2] = initialStates[i+2];\n    particleStatesOut[i+3] = initialStates[i+3];\n    particleStatesOut[i+4] = 1;\n  } else {\n    particleStatesOut[i] = positionOut.x;\n    particleStatesOut[i+1] = positionOut.y;\n    particleStatesOut[i+2] = velocityOut.x;\n    particleStatesOut[i+3] = velocityOut.y;\n    particleStatesOut[i+4] = lifespan;\n  }\n}";
+
+
+var $e763cc0f2c34ff9e$exports = {};
+$e763cc0f2c34ff9e$exports = "struct VertexOutput {\n  @builtin(position) pos: vec4f,\n  @location(1) alpha: f32,\n};\n\n@vertex\nfn vertexMain(@location(0) pos: vec2f) -> VertexOutput {\n\n  var output: VertexOutput;\n  output.pos = vec4f(pos.x * 2, pos.y * 2 + ${ORIGIN_Y}, 0, 1);\n  if (pos.x == 0 && pos.y == 0) {\n    output.alpha = 1;\n  } else {\n    output.alpha = 0;\n  }\n  return output;\n}\n\n@fragment\nfn fragmentMain(input: VertexOutput) -> @location(0) vec4f {\n  return vec4f(1,0.8,0.8,1) * input.alpha * 0.35; // rgba\n}";
+
+
+const $411d9a320534a5da$var$withConstants = (shader)=>shader.replace(/\$\{([A-Z_]+)\}/g, (_, variableName)=>$1ba33acf88ea8827$exports[variableName]);
+const $411d9a320534a5da$export$d495e82be185645c = $411d9a320534a5da$var$withConstants((0, (/*@__PURE__*/$parcel$interopDefault($1c923f75c1506daf$exports))));
+const $411d9a320534a5da$export$efec088117b02065 = $411d9a320534a5da$var$withConstants((0, (/*@__PURE__*/$parcel$interopDefault($45af1b9190be3989$exports))));
+const $411d9a320534a5da$export$ae8a6f9c9c107e7d = $411d9a320534a5da$var$withConstants((0, (/*@__PURE__*/$parcel$interopDefault($e763cc0f2c34ff9e$exports))));
+
+
+const $826c8f98e2b7c15f$var$TRIANGLE_COUNT = 16;
+const $826c8f98e2b7c15f$var$THETA = 2 * Math.PI / $826c8f98e2b7c15f$var$TRIANGLE_COUNT;
+const $826c8f98e2b7c15f$var$VERTICES = new Float32Array($826c8f98e2b7c15f$var$TRIANGLE_COUNT * 6); // 6 for the x,y coordinates of the three points on the triangle
+for(let triangleIndex = 0; triangleIndex < $826c8f98e2b7c15f$var$TRIANGLE_COUNT; triangleIndex++){
     const i = triangleIndex * 6;
-    const point1 = (0, $4e939e94d233ed5b$export$eb912ff306213ab3)(1, triangleIndex * $c8a6c04c3ac47780$var$THETA);
-    const point2 = (0, $4e939e94d233ed5b$export$eb912ff306213ab3)(1, (triangleIndex + 1) * $c8a6c04c3ac47780$var$THETA);
-    $c8a6c04c3ac47780$var$VERTICES[i] = 0;
-    $c8a6c04c3ac47780$var$VERTICES[i + 1] = 0;
-    $c8a6c04c3ac47780$var$VERTICES[i + 2] = point1.x;
-    $c8a6c04c3ac47780$var$VERTICES[i + 3] = point1.y;
-    $c8a6c04c3ac47780$var$VERTICES[i + 4] = point2.x;
-    $c8a6c04c3ac47780$var$VERTICES[i + 5] = point2.y;
+    const point1 = (0, $a208a15eab1643c4$export$eb912ff306213ab3)(1, triangleIndex * $826c8f98e2b7c15f$var$THETA);
+    const point2 = (0, $a208a15eab1643c4$export$eb912ff306213ab3)(1, (triangleIndex + 1) * $826c8f98e2b7c15f$var$THETA);
+    $826c8f98e2b7c15f$var$VERTICES[i] = 0;
+    $826c8f98e2b7c15f$var$VERTICES[i + 1] = 0;
+    $826c8f98e2b7c15f$var$VERTICES[i + 2] = point1.x;
+    $826c8f98e2b7c15f$var$VERTICES[i + 3] = point1.y;
+    $826c8f98e2b7c15f$var$VERTICES[i + 4] = point2.x;
+    $826c8f98e2b7c15f$var$VERTICES[i + 5] = point2.y;
 }
-class $c8a6c04c3ac47780$export$b9202086c45b387e extends (0, $50ad2468206126ad$export$462bb059fed9d9e5) {
+class $826c8f98e2b7c15f$export$b9202086c45b387e extends (0, $50ad2468206126ad$export$462bb059fed9d9e5) {
     setup() {
         const particleCounter = document.getElementById("particle-count");
-        if (particleCounter) particleCounter.textContent = `${Math.floor($c8a6c04c3ac47780$var$PARTICLE_COUNT / 1000)}k`;
+        if (particleCounter) particleCounter.textContent = `${Math.floor((0, $1ba33acf88ea8827$export$ffc0241869fe445) / 1000)}k`;
         /*
     ┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑
     │ PARTICLE SHAPE                  │
     ┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
     */ this.vertexBuffer = this.device.createBuffer({
             label: "Particle Vertices",
-            size: $c8a6c04c3ac47780$var$VERTICES.byteLength,
+            size: $826c8f98e2b7c15f$var$VERTICES.byteLength,
             usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
         });
-        this.device.queue.writeBuffer(this.vertexBuffer, 0, $c8a6c04c3ac47780$var$VERTICES);
+        this.device.queue.writeBuffer(this.vertexBuffer, 0, $826c8f98e2b7c15f$var$VERTICES);
         const vertexBufferLayout = {
             arrayStride: 8,
             attributes: [
@@ -156,12 +197,12 @@ class $c8a6c04c3ac47780$export$b9202086c45b387e extends (0, $50ad2468206126ad$ex
      *   lifespan
      * ]
      *
-     */ const particleStates = new Float32Array($c8a6c04c3ac47780$var$PARTICLE_COUNT * $c8a6c04c3ac47780$var$PARTICLE_INTERVAL);
-        for(let i = 0; i < particleStates.length; i += $c8a6c04c3ac47780$var$PARTICLE_INTERVAL){
-            const position = (0, $4e939e94d233ed5b$export$c4293a6a9b6ab33a)($c8a6c04c3ac47780$var$ORIGIN_RADIUS);
-            const velocity = (0, $4e939e94d233ed5b$export$c4293a6a9b6ab33a)($c8a6c04c3ac47780$var$ORIGIN_RADIUS);
+     */ const particleStates = new Float32Array((0, $1ba33acf88ea8827$export$ffc0241869fe445) * (0, $1ba33acf88ea8827$export$9065f2e29538d195));
+        for(let i = 0; i < particleStates.length; i += (0, $1ba33acf88ea8827$export$9065f2e29538d195)){
+            const position = (0, $a208a15eab1643c4$export$c4293a6a9b6ab33a)((0, $1ba33acf88ea8827$export$ff0d559910d80bb9));
+            const velocity = (0, $a208a15eab1643c4$export$c4293a6a9b6ab33a)((0, $1ba33acf88ea8827$export$ff0d559910d80bb9));
             particleStates[i] = position.x;
-            particleStates[i + 1] = position.y + $c8a6c04c3ac47780$var$ORIGIN_Y;
+            particleStates[i + 1] = position.y + (0, $1ba33acf88ea8827$export$28b92f9a30b92e0b);
             particleStates[i + 2] = velocity.x;
             particleStates[i + 3] = velocity.y;
             particleStates[i + 4] = Math.random();
@@ -277,38 +318,7 @@ class $c8a6c04c3ac47780$export$b9202086c45b387e extends (0, $50ad2468206126ad$ex
     ┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
     */ const computeShaderModule = this.device.createShaderModule({
             label: "Flame compute shader",
-            code: /* wgsl */ `
-        @group(0) @binding(0) var<storage> particleStatesIn: array<f32>;
-        @group(0) @binding(1) var<storage, read_write> particleStatesOut: array<f32>;
-        @group(0) @binding(2) var<storage> initialStates: array<f32>;
-
-        @compute
-        @workgroup_size(${$c8a6c04c3ac47780$var$WORKGROUP_SIZE})
-        fn computeMain(@builtin(global_invocation_id) index: vec3u) {
-          let i = index.x * ${$c8a6c04c3ac47780$var$PARTICLE_INTERVAL};
-          let positionIn = vec2f(particleStatesIn[i], particleStatesIn[i+1]);
-          let velocityIn = vec2f(particleStatesIn[i+2], particleStatesIn[i+3]);
-          let lifespan = particleStatesIn[i+4] - ${$c8a6c04c3ac47780$var$DECAY_RATE};
-
-          let positionOut = positionIn + velocityIn * ${$c8a6c04c3ac47780$var$SPEED};
-          let velocityOut = vec2f(velocityIn.x, velocityIn.y + ${$c8a6c04c3ac47780$var$GRAVITY});
-
-          // reset the particle
-          if (lifespan < 0) {
-            particleStatesOut[i] = initialStates[i];
-            particleStatesOut[i+1] = initialStates[i+1];
-            particleStatesOut[i+2] = initialStates[i+2];
-            particleStatesOut[i+3] = initialStates[i+3];
-            particleStatesOut[i+4] = 1;
-          } else {
-            particleStatesOut[i] = positionOut.x;
-            particleStatesOut[i+1] = positionOut.y;
-            particleStatesOut[i+2] = velocityOut.x;
-            particleStatesOut[i+3] = velocityOut.y;
-            particleStatesOut[i+4] = lifespan;
-          }
-        }
-      `
+            code: (0, $411d9a320534a5da$export$efec088117b02065)
         });
         this.computePipeline = this.device.createComputePipeline({
             label: "Flame compute pipeline",
@@ -324,62 +334,7 @@ class $c8a6c04c3ac47780$export$b9202086c45b387e extends (0, $50ad2468206126ad$ex
     ┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
     */ const renderShaderModule = this.device.createShaderModule({
             label: "Flame render shader",
-            code: /* wgsl */ `
-        struct VertexInput {
-          @location(0) pos: vec2f,
-          @builtin(instance_index) instance: u32,
-        };
-
-        struct VertexOutput {
-          @builtin(position) pos: vec4f,
-          @location(0) lifespan: f32,
-          @location(1) alpha: f32,
-        };
-
-        @group(0) @binding(0) var<storage> particleStates: array<f32>;
-
-        // Returns a y value between 0 and 1
-        fn lerp(x: f32, x1: f32, x2: f32, y1: f32, y2: f32) -> f32 {
-          return y1 + (x - x1) * (y2 - y1) / (x2 - x1);
-        }
-
-        fn particleScale(lifespan: f32) -> f32 {
-          let fullSizeBegin = 0.85;
-          let fullSizeEnd = 0.8;
-
-          if (lifespan > fullSizeBegin) {
-            return lerp(lifespan, 1, fullSizeBegin, 0, 1);
-          } else if (lifespan > fullSizeEnd) {
-            return 1;
-          } else {
-            return lerp(lifespan, fullSizeEnd, 0, 1, 0);
-          }
-        }
-        
-        @vertex
-        fn vertexMain(input: VertexInput) -> VertexOutput {
-          let i = input.instance * ${$c8a6c04c3ac47780$var$PARTICLE_INTERVAL};
-          let position = vec2f(particleStates[i], particleStates[i+1]);
-          let lifespan = particleStates[i+4];
-          let scale = particleScale(lifespan);
-          let pos = (input.pos * ${$c8a6c04c3ac47780$var$PARTICLE_SIZE} * scale) + position;
-
-          var output: VertexOutput;
-          output.pos = vec4f(pos, 0, 1);
-          output.lifespan = lifespan;
-          if (input.pos.x == 0 && input.pos.y == 0) {
-            output.alpha = scale;
-          } else {
-            output.alpha = 0;
-          }
-          return output;
-        }
-
-        @fragment
-        fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
-          return vec4f(0.4 + input.lifespan * 2, input.lifespan * 1.5, input.lifespan * 2 - 0.75, 1) * input.alpha; // rgba
-        }
-      `
+            code: (0, $411d9a320534a5da$export$d495e82be185645c)
         });
         this.renderPipeline = this.device.createRenderPipeline({
             label: "Flame render pipeline",
@@ -413,30 +368,7 @@ class $c8a6c04c3ac47780$export$b9202086c45b387e extends (0, $50ad2468206126ad$ex
         });
         const backgroundShaderModule = this.device.createShaderModule({
             label: "Background render shader",
-            code: /* wgsl */ `
-        struct VertexOutput {
-          @builtin(position) pos: vec4f,
-          @location(1) alpha: f32,
-        };
-        
-        @vertex
-        fn vertexMain(@location(0) pos: vec2f) -> VertexOutput {
-
-          var output: VertexOutput;
-          output.pos = vec4f(pos.x * 2, pos.y * 2 + ${$c8a6c04c3ac47780$var$ORIGIN_Y}, 0, 1);
-          if (pos.x == 0 && pos.y == 0) {
-            output.alpha = 1;
-          } else {
-            output.alpha = 0;
-          }
-          return output;
-        }
-
-        @fragment
-        fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
-          return vec4f(1,0.8,0.8,1) * input.alpha * 0.35; // rgba
-        }
-      `
+            code: (0, $411d9a320534a5da$export$ae8a6f9c9c107e7d)
         });
         this.backgroundPipeline = this.device.createRenderPipeline({
             label: "Background render pipeline",
@@ -475,7 +407,7 @@ class $c8a6c04c3ac47780$export$b9202086c45b387e extends (0, $50ad2468206126ad$ex
         const computePass = encoder.beginComputePass();
         computePass.setPipeline(this.computePipeline);
         computePass.setBindGroup(0, this.bindGroups[this.step % 2]);
-        const workgroupCount = Math.ceil($c8a6c04c3ac47780$var$PARTICLE_COUNT / $c8a6c04c3ac47780$var$WORKGROUP_SIZE);
+        const workgroupCount = Math.ceil((0, $1ba33acf88ea8827$export$ffc0241869fe445) / (0, $1ba33acf88ea8827$export$fb78680ee3bd0d21));
         computePass.dispatchWorkgroups(workgroupCount, workgroupCount);
         computePass.end();
         this.step++;
@@ -497,11 +429,11 @@ class $c8a6c04c3ac47780$export$b9202086c45b387e extends (0, $50ad2468206126ad$ex
         });
         pass.setPipeline(this.backgroundPipeline);
         pass.setVertexBuffer(0, this.vertexBuffer);
-        pass.draw($c8a6c04c3ac47780$var$VERTICES.length / 2);
+        pass.draw($826c8f98e2b7c15f$var$VERTICES.length / 2);
         pass.setPipeline(this.renderPipeline);
         pass.setVertexBuffer(0, this.vertexBuffer);
         pass.setBindGroup(0, this.bindGroups[this.step % 2]);
-        pass.draw($c8a6c04c3ac47780$var$VERTICES.length / 2, $c8a6c04c3ac47780$var$PARTICLE_COUNT);
+        pass.draw($826c8f98e2b7c15f$var$VERTICES.length / 2, (0, $1ba33acf88ea8827$export$ffc0241869fe445));
         pass.end();
         // Finish the command buffer and immediately submit it.
         this.device.queue.submit([
@@ -516,7 +448,7 @@ class $c8a6c04c3ac47780$export$b9202086c45b387e extends (0, $50ad2468206126ad$ex
 
 
 const $35d6c5b58b8fcd66$var$canvas = document.querySelector("canvas");
-new (0, $c8a6c04c3ac47780$export$b9202086c45b387e)($35d6c5b58b8fcd66$var$canvas);
+new (0, $826c8f98e2b7c15f$export$b9202086c45b387e)($35d6c5b58b8fcd66$var$canvas);
 
 
-//# sourceMappingURL=index.d5edbc8f.js.map
+//# sourceMappingURL=index.5a79ff3d.js.map
